9abc9693e70c5a8e0edf5abf0ad6d99f474badfa
Rostyslav <roosolam@gmail.com>
2025-11-23 14:16:35 +0200

    users models

---
+++ apps/users/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+
+class CustomUser(AbstractUser):
+    email = models.EmailField(unique=True)
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = ['username']
+
+
+    def __str__(self):
+        return self.email
